3b3c3c8652c59d9f0f7c39006d9bfac4a636bb3c
miguelv90210 <57293179+miguelv90210@users.noreply.github.com>
2020-07-06 00:46:19 -0600

    correo v1

---
+++ app/src/main/java/ues/fia/eisi/reservalocalfia/Utils.java
@@ -0,0 +1,7 @@
+package ues.fia.eisi.reservalocalfia;
+
+public class Utils {
+    public static final String EMAIL = "grupopdm04@gmail.com";
+    public static final String PASSWORD ="PDM04321";
+
+}
