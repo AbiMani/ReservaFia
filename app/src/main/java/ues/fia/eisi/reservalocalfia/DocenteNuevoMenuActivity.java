a29ec9c2804f509c972106fd6f4548b123c0732e
aby <34138848+AbiMani@users.noreply.github.com>
2020-07-08 15:14:29 -0600

    cambio menu docente

--- app/src/main/java/ues/fia/eisi/reservalocalfia/DocenteNuevoMenuActivity.java
+++ app/src/main/java/ues/fia/eisi/reservalocalfia/DocenteNuevoMenuActivity.java
@@ -5,45 +5,110 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class DocenteNuevoMenuActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+public class DocenteNuevoMenuActivity  extends AppCompatActivity implements View.OnClickListener, GestureDetector.OnGestureListener {
+    private CardView reserva, detallereser, grupo, horario, carga, pdf;
+    private GestureDetector gestureScanner;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_docente_nuevo_menu);
+        gestureScanner = new GestureDetector(this);
+
+        reserva = (CardView) findViewById(R.id.res);
+        detallereser = (CardView) findViewById(R.id.deres);
+        grupo = (CardView) findViewById(R.id.gr);
+        horario = (CardView) findViewById(R.id.hor);
+        carga = (CardView) findViewById(R.id.rcar);
+        pdf = (CardView) findViewById(R.id.pdf);
+
+        reserva.setOnClickListener(this);
+        detallereser.setOnClickListener(this);
+        grupo.setOnClickListener(this);
+        horario.setOnClickListener(this);
+        carga.setOnClickListener(this);
+        pdf.setOnClickListener(this);
+
 
     }
     public void lanzarMenuD(View v) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent me) {
+        return gestureScanner.onTouchEvent(me);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Intent intent= new Intent(this,TipoLocalInsertarActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Intent intent= new Intent(this,EscuelaInsertarActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Intent intent= new Intent(this,DocenteInsertarActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
         Intent i = null;
         switch (v.getId()) {
-            case R.id.button_D1:
+            case R.id.res:
                 i = new Intent(this, ReservaEventoMenuActivity.class);
                 break;
-            case R.id.button_D2:
+            case R.id.deres:
                 i = new Intent(this,DetalleReservaMenuActivity.class);
                 break;
-            case R.id.button_D3:
+            case R.id.gr:
                 i = new Intent(this,GrupoMenuActivity.class);
                 break;
 
-            case R.id.button_D5:
+            case R.id.hor:
                 i = new Intent(this,HorarioMenuActivity.class);
                 break;
-            case R.id.button_D6:
+            case R.id.rcar:
                 i = new Intent(this,CargaAcademicaMenuActivity.class);
                 break;
-            case R.id.button_D7:
+            case R.id.pdf:
                 i = new Intent(this,ExportarPdfActivity.class);
                 break;
         }
         if (i != null) startActivity(i);
     }
-
-
 }
